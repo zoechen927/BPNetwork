82c33507807e843961cbae65545701d79a806dac
Zhuoyun Chen <112053244+zoechen927@users.noreply.github.com>
2023-02-27 21:20:33 -0600

    Add files via upload

--- BPNetwork.py
+++ BPNetwork.py
@@ -7,12 +7,12 @@ import numpy as np
 from PIL import Image
 import scipy.io as sio
 
-# 激活函数
+# activation function
 def sigmoid(x):
     return np.array(list(map(lambda i: 1 / (1 + np.exp(-i)), x)))
 
 def get_train_pattern():
-    # 返回训练集的特征和标签
+    # Return the features and labels of the training set
     current_dir = os.getcwd()
     # current_dir = "/home/lxp/F/developing_folder/intelligence_system/bpneuralnet/"
     # train = sio.loadmat(current_dir + "mnist_train.mat")["mnist_train"]
@@ -21,119 +21,119 @@ def get_train_pattern():
     train_label = sio.loadmat( current_dir + "/mnist_train_labels.mat")
     
     
-    train["mnist_train"]  = np.where(train["mnist_train"] > 180, 1, 0)  # 二值化
+    train["mnist_train"]  = np.where(train["mnist_train"] > 180, 1, 0)  # Binarization
     return train["mnist_train"] , train_label["mnist_train_labels"]
 
 
-# 测试集，每个数字对应的文件夹下的图片展开成一维向量，连接到一起，组成每个数字的训练集。一个数字为一行
+# For the test set, the pictures in the folder corresponding to each number are expanded into one-dimensional vectors, which are connected together to form the training set for each number. one number per line
 def get_test_pattern():
-    # 返回测试集
+    # return test set
     base_url = os.getcwd() + "/mnist_test/"
     # base_url = "/home/lxp/F/developing_folder/intelligence_system/bpneuralnet/mnist_test/"
     test_img_pattern = []
-    # 十个数字
+    # ten digits
     for i in range(10):
         img_url = os.listdir(base_url + str(i))
         t = []
         for url in img_url:
-            img = Image.open(base_url + str(i) + "/" + url) # 依次打开数字图片
-            img = img.convert('1')  # 二值化
-            img_array = np.asarray(img, 'i')  # 转化为int数组
+            img = Image.open(base_url + str(i) + "/" + url) # Open digital pictures in turn
+            img = img.convert('1')  # Binarization
+            img_array = np.asarray(img, 'i')  # convert to int array
             img_vector = img_array.reshape(
-                    img_array.shape[0] * img_array.shape[1])  # 展开成一维数组
-            t.append(img_vector) # 同一个数字展开成一维向量都存放在一个数组里
-        test_img_pattern.append(t) # 每个数字为一行，十个数字数组的地址放在这里面
+                    img_array.shape[0] * img_array.shape[1])  # expand into a one-dimensional array
+            t.append(img_vector) # The same number is expanded into a one-dimensional vector and stored in an array
+        test_img_pattern.append(t) # Each number is a line, and the addresses of the ten number arrays are placed here
     return test_img_pattern
 
 
 class BPNetwork:
-    # 神经网络类
+    # Neural network class
     def __init__(self, in_count, hiden_count, out_count, in_rate, hiden_rate):
         """
 
-        :param in_count:        输入层数
-        :param hiden_count:     隐藏层数
-        :param out_count:       输出层数
-        :param in_rate:         输入层学习率
-        :param hiden_rate:      隐藏层学习率
+        :param in_count:        number of input layers
+        :param hiden_count:     number of hidden layers
+        :param out_count:       number of output layers
+        :param in_rate:         input layer learning rate
+        :param hiden_rate:      hidden layer learning rate
         """
-        # 各个层的节点数量
+        # The number of nodes in each layer
         self.in_count = in_count
         self.hiden_count = hiden_count
         self.out_count = out_count
 
-        # 输入层到隐藏层连线的权重随机初始化
+        # The weights of the input layer to the hidden layer connection are randomly initialized
         self.w1 = 0.2 * \
                 np.random.random((self.in_count, self.hiden_count)) - 0.1
 
-        # 隐藏层到输出层连线的权重随机初始化
+        # The weights of the hidden layer to the output layer connection are randomly initialized
         self.w2 = 0.2 * \
                 np.random.random((self.hiden_count, self.out_count)) - 0.1
 
-        # 隐藏层偏置向量
+        # Hidden layer bias vector
         self.hiden_offset = np.zeros(self.hiden_count)
-        # 输出层偏置向量
+        # Output layer bias vector
         self.out_offset = np.zeros(self.out_count)
 
-        # 输入层学习率
+        # input layer learning rate
         self.in_rate = in_rate
-        # 隐藏层学习率
+        # hidden layer learning rate
         self.hiden_rate = hiden_rate
 
-# 训练集里每一行是一个图片的数据
-    def train(self, train_img_pattern, train_label):# 输入层，输入层的标签
-        if self.in_count != len(train_img_pattern[0]):# 每张图片的维数
-            sys.exit("输入层维数与样本维数不等")
+# Each row in the training set is the data of a picture
+    def train(self, train_img_pattern, train_label):# input layer, the label of the input layer
+        if self.in_count != len(train_img_pattern[0]):# Dimensions of each image
+            sys.exit("The input layer dimension is not equal to the sample dimension")
         # for num in range(10):
         # for num in range(10):
-        for m in range(1,6,1):#循环增加训练次数
+        for m in range(1,6,1):#Loop to increase training times
             for i in range(len(train_img_pattern)):
 
-                # 生成目标向量
+                # generate target vector
                 target = [0] * 10
-                target[train_label[i][0]] = 1 #train_label[i][0]代表是哪个数字，target[train_label[i][0]] = 1代表理想输出为1
+                target[train_label[i][0]] = 1 #train_label[i][0] represents which number it is，target[train_label[i][0]] = 1 represents ideal output is 1
 
-                # 前向传播
-                # 隐藏层值等于输入层*w1+隐藏层偏置
+                # forward propagation
+                # Hidden layer value is equal to input layer*w1+hidden layer bias
                 hiden_value = np.dot(train_img_pattern[i], self.w1) + self.hiden_offset
                 hiden_value = sigmoid(hiden_value)
 
-                # 计算输出层的输出
+                # Compute the output of the output layer
                 out_value = np.dot(hiden_value, self.w2) + self.out_offset
                 out_value = sigmoid(out_value)
 
-                # 反向更新
+                # backpropagation
                 error = target - out_value
-                # 计算输出层误差
+                # Calculate output layer error
                 out_error = out_value * (1 - out_value) * error
-                # 计算隐藏层误差
+                # Calculate hidden layer error
                 hiden_error = hiden_value * \
                         (1 - hiden_value) * np.dot(self.w2, out_error)
 
-                # 更新w2，w2是j行k列的矩阵，存储隐藏层到输出层的权值
+                # Update w2, w2 is a matrix of j rows and k columns, storing the weights from the hidden layer to the output layer
                 #
                 for k in range(self.out_count):
-                    # 更新w2第k列的值，连接隐藏层所有节点到输出层的第k个节点的边
-                    # 隐藏层学习率×输入层误差×隐藏层的输出值
-                    # 隐藏层学习率×输出层误差×隐藏层的输出值
+                    # Update the value of the kth column of w2, and connect all nodes of the hidden layer to the kth node of the output layer
+                    # Hidden layer learning rate × input layer error × output value of hidden layer
+                    # Hidden layer learning rate × output layer error × output value of hidden layer
                     self.w2[:, k] += self.hiden_rate * out_error[k] * hiden_value
 
-                # 更新w1 输入层到隐藏层的权重
+                # Update w1, the weight from the input layer to the hidden layer
                 for j in range(self.hiden_count):
-                    # 学习率*隐藏层的梯度*输入层的值
+                    # learning rate * gradient of hidden layer * value of input layer
                     self.w1[:, j] += self.in_rate * \
                             hiden_error[j] * train_img_pattern[i]
 
-                # 更新偏置向量
+                # update bias vector
                 self.out_offset += self.hiden_rate * out_error
                 self.hiden_offset += self.in_rate * hiden_error
 
-            # 该次循环训练成功
+            # The training cycle was successful
 
-            print("第"+str(m)+"次训练成功")
+            print("The "+str(m)+" time training succeed")
 
 
-        # 训练好的权重，偏移量保存下来
+        # The trained weights and offsets are saved
         sio.savemat(os.getcwd() + "/w1.mat",{'w1':self.w1})
         sio.savemat(os.getcwd() + "/w2.mat",{'w2':self.w2})
         sio.savemat(os.getcwd() + "/hiden_offset.mat",{'hiden_offset':self.hiden_offset})
@@ -141,50 +141,50 @@ class BPNetwork:
 
     def test(self, test_img_pattern):
         """
-        测试神经网络的正确率
-        :param test_img_pattern[num][t]表示数字num的第t张图片
+        Test the accuracy of the neural network
+        :param test_img_pattern[num][t] represents number num'th t time picture
         :return:
         """
         right = np.zeros(10)
         test_sum = 0
-        for num in range(10):  # 10个数字
-            # print("正在识别", num)
-            num_count = len(test_img_pattern[num]) # 每个数字对应的测试图像数量
+        for num in range(10):  # 10 numbers
+            # print("identifying", num)
+            num_count = len(test_img_pattern[num]) # The number of test images corresponding to each number
             test_sum += num_count
-            for t in range(num_count):  # 数字num的第t张图片
+            for t in range(num_count):  # number num'th t time picture
                 hiden_value = np.dot(
                         test_img_pattern[num][t], self.w1) + self.hiden_offset
                 hiden_value = sigmoid(hiden_value)
                 out_value = np.dot(hiden_value, self.w2) + self.out_offset
                 out_value = sigmoid(out_value)
                 # print(out_value)
-                if np.argmax(out_value) == num: # 索引：相似度，相似度最大的所对应的索引即为预测值
-                    # 识别正确
+                if np.argmax(out_value) == num: # Index: similarity, the index corresponding to the highest similarity is the predicted value
+                    # correct identification
                     right[num] += 1
-            print("数字%d的识别正确率%f" % (num, right[num] / num_count))
+            print("num%d identification accuracy is%f" % (num, right[num] / num_count))
 
-        # 平均识别率
-        print("平均识别率为：", sum(right) / test_sum)
+        # Average recognition rate
+        print("Average recognition rate is：", sum(right) / test_sum)
         return np.argmax(out_value)
 
 
 def run():
 
-    # 神经网络配置参数
+    # Neural Network Configuration Parameters
     in_count = 28 * 28
     hiden_count = 6
     out_count = 10
     in_rate = 0.02
     hiden_rate = 0.1
-    print("输入层、输出层学习率分别是:"+str(in_rate)+","+str(hiden_rate))
-    bpnn = BPNetwork(in_count, hiden_count, out_count, in_rate, hiden_rate)# 新建一个BP神经网络的对象
+    print("The learning rates of the input layer and the output layer are respectively:"+str(in_rate)+","+str(hiden_rate))
+    bpnn = BPNetwork(in_count, hiden_count, out_count, in_rate, hiden_rate)# Create a new BP neural network object
 
-    # 读入训练集
+    # read in the training set
     train, train_label = get_train_pattern()
 
-    # 读入测试图片
+    # read test image
     test_pattern = get_test_pattern()
-    bpnn.train(train, train_label) # 不存在则训练
+    bpnn.train(train, train_label) # train if not present
     bpnn.test(test_pattern)
 
 if __name__ == "__main__":
